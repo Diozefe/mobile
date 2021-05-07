c973c60b278e737617eb7b458ddcac24700bde0d
dmorais.eng.comp <dmorais.eng.comp@gmail.com>
2021-05-07 09:23:14 -0300

    Adicionado imagens. Alinhamento de filhos do Widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,64 @@
 import 'package:flutter/material.dart';
-import 'package:mobclinic/screens/transferencia/lista.dart';
 
-void main() => runApp(MobClinic());
+void main() => runApp(SlackApp());
 
-class MobClinic extends StatelessWidget {
+class SlackApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ListaTransferencias(),
       theme: ThemeData(
-        primaryColor: Colors.purple[400],
-        accentColor: Colors.purpleAccent[700],
+        primaryColor: Colors.blue[700],
+        accentColor: Colors.blue[300],
         buttonTheme: ButtonThemeData(
-          buttonColor: Colors.purpleAccent[600],
-          textTheme: ButtonTextTheme.primary,
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.accent,
         ),
+      ),
+      home: Dashboard(),
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/logo-slack.jpg'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: EdgeInsets.all(8.0),
+              height: 80,
+              width: 130,
+              color: Theme.of(context).primaryColor,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(
+                    Icons.people,
+                    color: Colors.white,
+                    size: 30.0,
+                  ),
+                  Text(
+                    'Contatos',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
